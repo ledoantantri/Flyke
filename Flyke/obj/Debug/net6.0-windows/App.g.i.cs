ea808dbfa17bcac753a043b519051d3c17239cc2
ledoantantri <ledoantantri@gmail.com>
2023-06-05 23:50:08 +0700

    autoupdate

--- Flyke/obj/Debug/net6.0-windows/App.g.i.cs
+++ Flyke/obj/Debug/net6.0-windows/App.g.i.cs
@@ -45,7 +45,7 @@ namespace Flyke {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.2.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.5.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace Flyke {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.2.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.5.0")]
         public static void Main() {
             Flyke.App app = new Flyke.App();
             app.InitializeComponent();
