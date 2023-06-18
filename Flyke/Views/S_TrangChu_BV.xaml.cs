80e3436ec7ef832cd4fe80ac65094ea402870021
ledoantantri <ledoantantri@gmail.com>
2023-06-19 02:50:20 +0700

    Move some files of ViKhang

---
+++ Flyke/Views/S_TrangChu_BV.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Flyke_FrontEnd.view
+{
+    /// <summary>
+    /// Interaction logic for S_HomeToBV.xaml
+    /// </summary>
+    public partial class S_HomeToBV : Window
+    {
+        public S_HomeToBV()
+        {
+            InitializeComponent();
+        }
+    }
+}
