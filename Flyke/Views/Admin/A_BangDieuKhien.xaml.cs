4abb8564819d1645a52f75ab1b1c95d3fd5b29ec
shenkha <21522189@ms.uit.edu.vn>
2023-06-19 22:20:55 +0700

    small fix on A_BangDieuKhien

--- Flyke/Views/Admin/A_BangDieuKhien.xaml.cs
+++ Flyke/Views/Admin/A_BangDieuKhien.xaml.cs
@@ -16,11 +16,11 @@ using System.Windows.Shapes;
 namespace A_Dashboard
 {
     /// <summary>
-    /// Interaction logic for UserControl1.xaml
+    /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class UserControl1 : UserControl
+    public partial class MainWindow : Window
     {
-        public UserControl1()
+        public MainWindow()
         {
             InitializeComponent();
         }
--- Flyke/obj/Debug/net6.0-windows/App.g.i.cs
+++ Flyke/obj/Debug/net6.0-windows/App.g.i.cs
@@ -47,7 +47,7 @@ namespace Flyke {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.5.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.2.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
@@ -59,7 +59,7 @@ namespace Flyke {
             
             #line default
             #line hidden
-            System.Uri resourceLocater = new System.Uri("/Flyke;component/app.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Flyke;V1.0.0.0;component/app.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\App.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
@@ -73,7 +73,7 @@ namespace Flyke {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.5.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.2.0")]
         public static void Main() {
             Flyke.App app = new Flyke.App();
             app.InitializeComponent();
